81c842a345109c7382d1d5e85709135ace8b5c68
Miguel Andres Herize Godoy <miguel.herize@mykukun.com>
2023-04-19 02:12:53 -0400

    add change styles and docstring in test

--- test/test_spider.py
+++ test/test_spider.py
@@ -1,9 +1,9 @@
-import sys
-
-sys.path.append("/Users/migherize/Sourcetree/TourismMadrid-1/")
+"""
+Test para webscrapy en https://turismomadrid.es/es/rutas.html con unittest
+author: Miguel Herize
+mail: migherize@gmail.com
+"""
 import unittest
-
-# from scrapy.utils.test import get_crawler
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from src.turismo_madrid.turismo_madrid.spiders.turismo_madrid_spider import (
@@ -12,13 +12,23 @@ from src.turismo_madrid.turismo_madrid.spiders.turismo_madrid_spider import (
 
 
 class TestScrapy(unittest.TestCase):
+    """
+    Clase para probar la araña TourismMadridSpider en la página https://turismomadrid.es/es/rutas.html
+    """
+
     def test_spider_runs_successfully(self):
+        """
+        Prueba que comprueba que la araña se ejecuta correctamente en la página web de Turismo Madrid.
+        """
+        # Configuramos el proceso de rastreo
         settings = get_project_settings()
         process = CrawlerProcess(settings)
-        process.crawl(
-            TourismMadridSpider
-        )  # Usamos la clase de la araña, no una instancia
+        
+        # Iniciamos el proceso de rastreo
+        process.crawl(TourismMadridSpider)
         process.start()
+
+        # Verificamos que la araña se ha ejecutado correctamente
         self.assertTrue(TourismMadridSpider.successful)
 
 
