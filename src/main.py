68afc016624558cbf8b61b13dd45329e695a9047
Miguel Andres Herize Godoy <miguel.herize@mykukun.com>
2023-04-20 12:32:58 -0400

    add endpoint get db

--- src/main.py
+++ src/main.py
@@ -7,8 +7,13 @@ mail: migherize@gmail.com
 import os
 import subprocess
 from fastapi import FastAPI
+from pymongo import MongoClient
+from ast import literal_eval
 
 app = FastAPI()
+
+client = MongoClient("mongodb://admin:password@localhost:27017/")
+db = client["RutasMadrid"]
 
 
 @app.get("/")
@@ -21,17 +26,73 @@ def home_page():
 def crawler():
     """
     Ejecuta un spider de Scrapy en el directorio del proyecto.
+
+    Returns:
+        dict: Un diccionario con el mensaje de éxito al ejecutar el spider.
+
+    Raises:
+        subprocess.CalledProcessError: Si el proceso del spider no se pudo ejecutar correctamente.
     """
     project_path = os.path.join(os.getcwd(), "turismo_madrid")
     spider_args = ["scrapy", "crawl", "turismo_madrid_spider"]
 
+    current_dir = os.getcwd()
+
     os.chdir(project_path)
 
     subprocess.run(spider_args, check=True)
-    os.chdir(os.getcwd())
+
+    os.chdir(current_dir)
     return {"message": "Spider ejecutado exitosamente"}
 
 
 @app.get("/Get-RutasMadrid")
 def get_all():
-    return {"message": "Spider ejecutado exitosamente"}
+    """Obtiene todas las rutas de Madrid almacenadas en MongoDB.
+
+    Returns:
+        list: Una lista de diccionarios que contienen la información de todas las rutas de Madrid en MongoDB.
+
+    Raises:
+        HTTPException: Si no se pudieron obtener las rutas de MongoDB.
+    """
+    list_result = []
+    resultados = db["router"].find()
+    for resultado in resultados:
+        if "list_stage_ids" in resultado:
+            lista_id_etapas = resultado["list_stage_ids"]
+            etapas = db["stages"].find({"_id": {"$in": lista_id_etapas}})
+            resultado["list_stage_ids"] = [etapa for etapa in etapas]
+            for resultado2 in resultado["list_stage_ids"]:
+                if "info_itinerarios" in resultado2:
+                    lista_id_itinerarys = resultado2["info_itinerarios"]
+                    itinerarys = db["itinerary"].find(
+                        {"_id": {"$in": lista_id_itinerarys}}
+                    )
+                    resultado2["info_itinerarios"] = [
+                        itinerary for itinerary in itinerarys
+                    ]
+                    continue
+
+                else:
+                    continue
+        else:
+            continue
+
+        list_result.append(resultado)
+
+    print(len(list_result))
+
+    def change_type(data: dict):
+        aux_data = data.copy()
+        for key, value in aux_data.items():
+            if isinstance(value, dict):
+                aux_data[key] = change_type(value)
+                continue
+            aux_data[key] = str(value)
+        return aux_data
+
+    for index, d in enumerate(list_result):
+        list_result[index] = change_type(d)
+
+    return list_result
